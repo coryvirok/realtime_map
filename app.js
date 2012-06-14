900c1405e91c9e4ca31f43c0734a9782686e401e
Cory Virok <coryvirok@gmail.com>
2012-06-14 12:43:32 -0700

    simplified

--- app.js
+++ app.js
@@ -24,79 +24,31 @@ function handler(req, res) {
 }
 
 io.configure( function() {
-  io.set('close timeout', 60*60*24); // 24h time out
+  io.set('close timeout', 60 * 60 * 24); // 24h time out
 });
 
-function SessionController (user) {
-  // session controller class for storing redis connections
-  // this is more a workaround for the proof-of-concept
-  // in "real" applications session handling should NOT
-  // be done like this
-  this.sub = redis.createClient(REDIS_PORT, REDIS_HOST, {maxReconnectionAttempts: 2});
-  this.pub = redis.createClient(REDIS_PORT, REDIS_HOST, {maxReconnectionAttempts: 2});
-
-  this.user = user;
-}
-
-SessionController.prototype.subscribe = function(socket) {
-  var sub = this.sub;
-  var client = sub.client;
-  var self = this;
-
-  console.log('SessionController.subscribe');
-
-  sub.on('message', function(channel, message) {
-    console.log('REDIS MESSAGE: ' + message);
-    socket.emit(channel, message);
-  });
-
-  sub.on('subscribe', function(channel, count) {
-    console.log('REDIS SUBSCRIBE count: ' + count);
-    socket.emit(channel, JSON.stringify({action: 'join', user: this.user});
-  });
-  console.log('subscribing to redis topic: ' + REDIS_TOPIC);
-  sub.subscribe(REDIS_TOPIC);
-};
-
-SessionController.prototype.unsubscribe = function() {
-  console.log('SessionController.unsubscribe');
-  this.sub.unsubscribe('map');
-};
-
-SessionController.prototype.publish = function(message) {
-  console.log('SessionController.publish: ' + message);
-  this.pub.publish('map', message);
-};
-
-SessionController.prototype.destroyRedis = function() {
-  console.log('SessionController.destroyRedis');
-  if (this.sub !== null) this.sub.quit();
-  if (this.pub !== null) this.pub.quit();
-};
-
 io.sockets.on('connection', function (socket) {
-  console.log('socket.io connection');
-  console.log(socket.id);
 
-  socket.on('join', function(data) {
-    console.log('socket.io join');
-    var msg = JSON.parse(data);
-    var sessionController = new SessionController(msg.user);
-    socket.set('sessionController', sessionController);
-    sessionController.subscribe(socket);
-    // just some logging to trace the chat data
-    console.log(data);
-  });
-
-  socket.on('disconnect', function() {
-    console.log('socket.io disconnect');
-    socket.get('sessionController', function(err, sessionController) {
-      if (sessionController != null) {
-        sessionController.unsubscribe();
-        var leaveMessage = JSON.stringify({action: 'leave', user: sessionController.user});
-        sessionController.publish(leaveMessage);
-        sessionController.destroyRedis();
+  socket.on('join', function(name) {
+    socket.get('session', function (err, session) {
+      if (session === null) {
+        var redisClient = redis.createClient(REDIS_PORT, REDIS_HOST, {maxReconnectionAttempts: 2});
+        socket.set('session', {redisClient: redisClient, name: name});
+        socket.emit('ready');
       }
     });
+  });
+
+  socket.on('disconnect', function () {
+    socket.get('session', function (err, session) {
+      console.log('Disconnect ', session.name);
+      if (session !== null) {
+        var client = session.redisClient;
+        if (client) {
+          client.quit();
+        }
+      }
+    });
+    socket.emit('ready');
   });
 });
