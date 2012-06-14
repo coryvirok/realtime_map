c69c13a32879d4f749027f5ab6a46d1795026c63
Lawrence Wang <lawrence@lumoslabs.com>
2012-06-14 16:44:22 -0700

    use d3 and an azimuthal projection

--- static/map.js
+++ static/map.js
@@ -1,19 +1,101 @@
+
+now.message = function(data) {
+  // console.log(data);
+}
+
 $(function() {
 
-  var map = new L.Map('map');
-  var cloudmade = new L.TileLayer('http://{s}.tile.cloudmade.com/151c95d495334f98ad1e346cbb7e87d6/65306/256/{z}/{x}/{y}.png', {
-      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
-      maxZoom: 18
+  var WIDTH = $(window).width(), HEIGHT = $(window).height();
+  var RADIUS = Math.min(WIDTH, HEIGHT)/2 - 20;
+
+  var feature, point;
+
+  var projection = d3.geo.azimuthal()
+      .scale(RADIUS)
+      .origin([-71.03,42.37])
+      .mode("orthographic")
+      .translate([WIDTH/2, HEIGHT/2]);
+
+  var circle = d3.geo.circle().origin(projection.origin());
+
+  var path = d3.geo.path().projection(projection);
+
+  var svg = d3.select("#map").append("svg:svg")
+      .attr("width", WIDTH)
+      .attr("height", HEIGHT)
+      .on("mousedown", mousedown);
+
+  svg.append('circle')
+    .attr('id', 'globe-background')
+    .attr('r', RADIUS)
+    .attr('cx', WIDTH/2)
+    .attr('cy', HEIGHT/2);
+
+  d3.json("world-countries.json", function(collection) {
+    feature = svg.selectAll("path")
+        .data(collection.features)
+      .enter().append("svg:path")
+        .attr("d", clip);
+
+    feature.append("svg:title").text(function(d) { return d.properties.name; });
+
+    var lumoslabs = {
+      "type": "Feature",
+      "properties": {"name":"Lumos Labs"},
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+          [-122.403743, 37.789577],
+          [-121.403743, 37.789577],
+          [-121.403743, 38.789577],
+          [-122.403743, 38.789577],
+          [-122.403743, 37.789577],
+        ]]
+      }
+    };
+
+    point = svg.append('svg:path').data([lumoslabs])
+      .attr('id', 'lumoslabs')
+      .attr('d', clip);
+
   });
-  var center_of_world = new L.LatLng(0,0); // geographical point (longitude and latitude)
-  map.setView(center_of_world, 2).addLayer(cloudmade);
-  var markerLocation = new L.LatLng(37.7895768, -122.403743);
 
-  var marker = new L.Marker(markerLocation);
-  map.addLayer(marker);
+  d3.select(window)
+      .on("mousemove", mousemove)
+      .on("mouseup", mouseup);
 
-  now.message = function(data) {
-    console.log(data);
+  var m0, o0;
+
+  function mousedown() {
+    m0 = [d3.event.pageX, d3.event.pageY];
+    o0 = projection.origin();
+    d3.event.preventDefault();
+  }
+
+  function mousemove() {
+    if (m0) {
+      var m1 = [d3.event.pageX, d3.event.pageY],
+          o1 = [o0[0] + (m0[0] - m1[0]) / 8, o0[1] + (m1[1] - m0[1]) / 8];
+      projection.origin(o1);
+      circle.origin(o1)
+      refresh();
+    }
+  }
+
+  function mouseup() {
+    if (m0) {
+      mousemove();
+      m0 = null;
+    }
+  }
+
+  function refresh(duration) {
+    (duration ? feature.transition().duration(duration) : feature).attr("d", clip);
+    (duration ? point.transition().duration(duration) : point).attr("d", clip);
+  }
+
+  function clip(d) {
+    return path(circle.clip(d));
   }
 
 });
