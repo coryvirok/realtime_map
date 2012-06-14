88dc9d3c41480cbd66b2442d12361ef914b77b41
Cory Virok <coryvirok@gmail.com>
2012-06-14 12:59:09 -0700

    moved stuff around

--- app.js
+++ app.js
@@ -3,23 +3,35 @@ REDIS_PORT = 6379;
 REDIS_TOPIC = 'events';
 
 var app = require('http').createServer(handler),
+    static = require('node-static'),
     io = require('socket.io').listen(app),
     fs = require('fs'),
-    redis = require('redis');
+    redis = require('redis'),
+    util = require('util');
+
+var webroot = './static';
+var file = new(static.Server)(webroot, {
+  cache: 600,
+  headers: {'X-Powered-By': 'Team Jeans'}
+});
 
 app.listen(5000);
 
 function handler(req, res) {
-  // just return the index HTML
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
-
-    res.writeHead(200);
-    res.end(data);
+  req.addListener('end', function() {
+    file.serve(req, res, function(err, result) {
+      if (err) {
+        console.error('Error serving %s - %s', req.url, err.message);
+        if (err.status === 404 || err.status === 500) {
+          file.serveFile(util.format('/%d.html', err.status), err.status, {}, req, res);
+        } else {
+          res.writeHead(err.status, err.headers);
+          res.end();
+        }
+      } else {
+        console.log('%s - %s', req.url, res.message);
+      }
+    });
   });
 }
 
